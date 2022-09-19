88fd9da5e47d76be2cfcad758198b42b0f3ed75e
beizhang <bei.zhang2019@gmail.com>
2022-09-19 15:46:26 +1000

    frame done

--- main.py
+++ main.py
@@ -50,14 +50,15 @@ else:
 
 # 输入车牌号
 rego = input("Please enter your rego number: ")
+
 # 选择要去的商店
 while True:
     store = int(input("Please choose the store you will visit (1/2): 1. Coles 2. Woolworths: "))
     if store == 1:
-        print(f"Please park your car at zone A number {num + 1}")
+        print(f"Please park your car at zone A NO. {num + 1}")
         break
     elif store == 2:
-        print(f"Please park your car at zone B number {num +1}")
+        print(f"Please park your car at zone B NO. {num +1}")
         break
     else:
         print("Invalid values please try again.")
@@ -70,46 +71,80 @@ print(f"Your check in time is {p1.time_in}")
 print('--------------------------------------------------------------------------------')
 print("Now you're finish shopping and ready to go home -_-")
 
+while True:
+    temp_out = input("Ready to go home? (yes/no): ")
+    if temp_out == "yes":
+        print(f"Your total parking time is: {time_in}")
+        print(f"Your total parking time is: {total_time}, you need to pay ${total_time * 3}.")
+        break
+    elif temp_out == "no":
+        print("Have fun!")
+        time.sleep(3)
+        continue
+    else:
+        print("Invalid values please try again.")
 
+# 输入车牌以显示车辆停放位置
+while True:
+    sec_rego = input("Please enter your rego number: ")
+    if sec_rego == rego and store == 1:
+        print(f"Your vehicles is at zone A NO. {num + 1}")
+        break
+    elif sec_rego == rego and store == 2:
+        print(f"Your vehicles is at zone B NO. {num + 1}")
+        break
+    else:
+        print("Invalid values please try again.")
+
+
+# 输入时间以确定是否为免费日
+date = int(input("What is today's date?(number only): "))
 if date == 1:
-    date = int(input("What is today's date?(number only): "))
     print("Today is the monthly membership day with free parking, have a nice day!")
     exit()
+# 非会员结算
 elif date != 1 and member == "no" and sec_member == "no":
-
     while True:
-        temp_out = input("Ready to go home? (yes/no): ")
-        if temp_out == "yes":
-            print(f"Your total parking time is: {time_in}")
-            print(f"Your total parking time is: {total_time}, you need to pay ${total_time * 3}.")
-
+        temp_check_out = input("Do you wish to continue? (y/n): ")
+        if temp_check_out == "y":
             while True:
-                temp_check_out = input("Do you wish to continue? (y/n): ")
-                if temp_check_out == "y":
-
-                    while True:
-                        temp_top_up = int(input("Please enter the amount of money you want to top up: "))
-                        if temp_top_up >= {total_time * 3}:
-                            print("Thanks for shopping today, have a nice day!")
-                            exit()
-                        elif temp_top_up < {total_time * 3}:
-                            print("Sorry, your top up money is not enough, please try again.")
-                        else:
-                            print("Invalid values please try again.")
-
-                elif temp_check_out == "no":
-                    continue
+                temp_top_up_money = int(input("Please enter the amount of money you want to top up: "))
+                if temp_top_up_money >= {total_time * 3}:
+                    print("Thanks for shopping today, have a nice day!")
+                    exit()
+                elif temp_top_up_money < {total_time * 3}:
+                    print("Sorry, your top up money is not enough, please try again.")
                 else:
                     print("Invalid values please try again.")
-
-        elif temp_out == "no":
-            print("Have fun!")
-            time.sleep(3)
+        elif temp_check_out == "no":
+            continue
         else:
             print("Invalid values please try again.")
 
+# 会员结算
 elif date != 1 and member == "yes" or sec_member == "yes":
-    
+    while True:
+        member_check_out_option = p1.member_check_out_menu()
+        if member_check_out_option == 1:
+            p1.member_check()
+            continue
+        elif member_check_out_option == 2:
+            top_up_money = int(input("Please enter the amount you want to top up: "))
+            p1.member_top_up(top_up_money)
+            continue
+        elif member_check_out_option == 3:
+            while True:
+                saving = p1.member_pay_bill()
+                if saving >= 0:
+                    print(f"The payment was successful and your balance is {saving}")
+                    print("Thank you for visiting today and see you next time!")
+                    exit()
+                else: 
+                    print("Sorry your balance is not enough please top up")
+                    continue
+        else:
+            print(" Invalid values please try again.")
 
-# else:
-#     print("Invalid values please try again.")
+# 输入错误
+else:
+    print("Invalid values please try again.")
--- p1.py
+++ p1.py
@@ -2,7 +2,7 @@
 #     def __init__(self, plate, Identity):
 #         self.plate = plate
 #         self.Identity = Identity
-import time, datetime
+import time, datetime,random
 
 # def member_select():
 #     member = input("Are you a member? (yes/no): ")
@@ -32,17 +32,17 @@ import time, datetime
 #     return time_in.
 
 
-class Time:
-    def __init__(self, time, ):
-        self.time = time
+# class Time:
+#     def __init__(self, time, ):
+#         self.time = time
         # self.finish_time = finish_time
         # self.total_time = total_time
 
-    def start_time(self):
+    # def start_time(self):
 # time_in = time.asctime(time.localtime(time.time()))
-        self.start_time = time.ctime()
-time_in = Time()
-time_in.start_time()
+#         self.start_time = time.ctime()
+# time_in = Time()
+# time_in.start_time()
     # return time_in
 # timeStruct = time.localtime(time_in)
 # strtime = time.strftime("%Y-%m-%d %H:%M:%S",timeStruct)
@@ -54,3 +54,31 @@ time_in.start_time()
         # self.total_time = 
 
 # print(self.start_time())
+
+
+
+
+def member_check_out_menu():
+    print("----------------------------------------------------------------")
+    print("Welcome to the member checkout page, please select the following options.")
+    print("check balance enter [1]")
+    print("top up enter [2]")
+    print("pay bill enter [3]")
+    return input("Please enter your select: ")
+
+def member_check():
+    global account_money
+    account_money = random.randint(0,30)
+    print(f"Hello {rogo}, your balance is ${account_money}.")
+
+def member_top_up(top_up_money):
+    global account_money
+    account_money += top_up_money
+    print(f"Hello {rego},You successfully deposited ${top_up_money}")
+    member_check()
+
+def member_pay_bill():
+    global account_money
+    account_money -= {total_time * 2}
+    return account_money
+    
