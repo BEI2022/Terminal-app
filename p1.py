ae5723150547398e4e1ea0574d7ca21f0560847f
beizhang <bei.zhang2019@gmail.com>
2022-09-19 22:56:18 +1000

    time

--- p1.py
+++ p1.py
@@ -2,60 +2,38 @@
 #     def __init__(self, plate, Identity):
 #         self.plate = plate
 #         self.Identity = Identity
-import time, datetime,random
+import datetime, random
 
-# def member_select():
-#     member = input("Are you a member? (yes/no): ")
-#     return member
+def member_select():
+    member = input("Are you a member? (yes/no): ")
+    return member
 
-# def sec_member_select():
-#     sec_member = input("As a member you can enjoy free car wash and free food, there are also discounts on parking fees, do you want to join the membership?: ")
-#     return sec_member
+def sec_member_select():
+    sec_member = input("As a member you can enjoy free car wash and free food, there are also discounts on parking fees, do you want to join the membership?: ")
+    return sec_member
 
-# def member_services():
-#     while True:
-#         option = int(input("As a member you can choose the following services: 1. free food 2. free car wash 3. other(1/2/3)"))
-#         if option == 1:
-#             print("Please pick up your food at the front door.")
-#             break
-#         elif option == 2:
-#             print("Please park your car in the car wash area")
-#             break
-#         elif option == 3:
-#             print("Sorry! this option is still under development, please select the other two options.")
-#             continue
-#         else:
-#             print("Invalid values please try again.")
+def member_services():
+    while True:
+        option = int(input("As a member you can choose the following services: 1. free food 2. free car wash 3. other(1/2/3)"))
+        if option == 1:
+            print("Please pick up your food at the front door.")
+            break
+        elif option == 2:
+            print("Please park your car in the car wash area")
+            break
+        elif option == 3:
+            print("Sorry! this option is still under development, please select the other two options.")
+            continue
+        else:
+            print("Invalid values please try again.")
 
-# def time_in():
-#     time_in = time.asctime(time.localtime(time.time()))
-#     return time_in.
+def time_in():
+    time_in = datetime.datetime.now()
+    return time_in
 
-
-# class Time:
-#     def __init__(self, time, ):
-#         self.time = time
-        # self.finish_time = finish_time
-        # self.total_time = total_time
-
-    # def start_time(self):
-# time_in = time.asctime(time.localtime(time.time()))
-#         self.start_time = time.ctime()
-# time_in = Time()
-# time_in.start_time()
-    # return time_in
-# timeStruct = time.localtime(time_in)
-# strtime = time.strftime("%Y-%m-%d %H:%M:%S",timeStruct)
-    # def finish_time(self):
-    #     self.finish_time += start_time() 
-
-
-    # def total_time(self):
-        # self.total_time = 
-
-# print(self.start_time())
-
-
+def time_out():
+    time_out = datetime.datetime.now()
+    return time_out
 
 
 def member_check_out_menu():
@@ -69,7 +47,7 @@ def member_check_out_menu():
 def member_check():
     global account_money
     account_money = random.randint(0,30)
-    print(f"Hello {rogo}, your balance is ${account_money}.")
+    print(f"Hello {rego}, your balance is ${account_money}.")
 
 def member_top_up(top_up_money):
     global account_money
---
+++ test.py
@@ -0,0 +1,14 @@
+import datetime
+def time_in():
+#     time_in = datetime.now()
+    
+
+
+    time_in = datetime.datetime.now()
+    return time_in
+    
+
+
+
+
+print(time_in())
