3f064ca930dd53051c5223fd7c959e5b9699ee6e
beizhang <bei.zhang2019@gmail.com>
2022-09-19 13:01:15 +1000

    temp check out done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import vehicles 
-import time, random, p1
+import random, p1,time
 
 # chen = vehicles.Vehicle('668-yyl', "member")
 
@@ -7,6 +7,7 @@ store = None
 member = None
 sec_member = None
 date = None
+
 
 
 # 显示欢迎语
@@ -24,6 +25,7 @@ if num == 20:
         exit()
     else:
         print("Invalid values please try again.")
+# 如果有空位
 else:
     # 选择是否是会员
     while True:
@@ -46,9 +48,9 @@ else:
             print("Invalid values please try again.")
 
 
-# 选择要去的商店
 # 输入车牌号
 rego = input("Please enter your rego number: ")
+# 选择要去的商店
 while True:
     store = int(input("Please choose the store you will visit (1/2): 1. Coles 2. Woolworths: "))
     if store == 1:
@@ -61,8 +63,8 @@ while True:
         print("Invalid values please try again.")
 
 # 记录到访时间
-time_in = time.asctime(time.localtime(time.time()))
-print(f"Your check in time is {time_in}")
+p1.time_in()
+print(f"Your check in time is {p1.time_in}")
 
 # check out part
 print('--------------------------------------------------------------------------------')
@@ -73,8 +75,41 @@ if date == 1:
     date = int(input("What is today's date?(number only): "))
     print("Today is the monthly membership day with free parking, have a nice day!")
     exit()
-# elif date != 1 and menber == no and sec_member == no:
+elif date != 1 and member == "no" and sec_member == "no":
+
+    while True:
+        temp_out = input("Ready to go home? (yes/no): ")
+        if temp_out == "yes":
+            print(f"Your total parking time is: {time_in}")
+            print(f"Your total parking time is: {total_time}, you need to pay ${total_time * 3}.")
+
+            while True:
+                temp_check_out = input("Do you wish to continue? (y/n): ")
+                if temp_check_out == "y":
+
+                    while True:
+                        temp_top_up = int(input("Please enter the amount of money you want to top up: "))
+                        if temp_top_up >= {total_time * 3}:
+                            print("Thanks for shopping today, have a nice day!")
+                            exit()
+                        elif temp_top_up < {total_time * 3}:
+                            print("Sorry, your top up money is not enough, please try again.")
+                        else:
+                            print("Invalid values please try again.")
+
+                elif temp_check_out == "no":
+                    continue
+                else:
+                    print("Invalid values please try again.")
+
+        elif temp_out == "no":
+            print("Have fun!")
+            time.sleep(3)
+        else:
+            print("Invalid values please try again.")
+
+elif date != 1 and member == "yes" or sec_member == "yes":
     
-    
+
 # else:
 #     print("Invalid values please try again.")
--- p1.py
+++ p1.py
@@ -2,28 +2,55 @@
 #     def __init__(self, plate, Identity):
 #         self.plate = plate
 #         self.Identity = Identity
+import time, datetime
 
-def member_select():
-    member = str(input("Are you a member? (yes/no): "))
-    return member
+# def member_select():
+#     member = input("Are you a member? (yes/no): ")
+#     return member
 
-def sec_member_select():
-    sec_member = str(input("As a member you can enjoy free car wash and free food, there are also discounts on parking fees, do you want to join the membership?: "))
-    return sec_member
+# def sec_member_select():
+#     sec_member = input("As a member you can enjoy free car wash and free food, there are also discounts on parking fees, do you want to join the membership?: ")
+#     return sec_member
 
-def member_services():
-    while True:
-        option = int(input("As a member you can choose the following services: 1. free food 2. free car wash 3. other(1/2/3)"))
-        if option == 1:
-            print("Please pick up your food at the front door.")
-            break
-        elif option == 2:
-            print("Please park your car in the car wash area")
-            break
-        elif option == 3:
-            print("Sorry! this option is still under development, please select the other two options.")
-            continue
-        else:
-            print("Invalid values please try again.")
+# def member_services():
+#     while True:
+#         option = int(input("As a member you can choose the following services: 1. free food 2. free car wash 3. other(1/2/3)"))
+#         if option == 1:
+#             print("Please pick up your food at the front door.")
+#             break
+#         elif option == 2:
+#             print("Please park your car in the car wash area")
+#             break
+#         elif option == 3:
+#             print("Sorry! this option is still under development, please select the other two options.")
+#             continue
+#         else:
+#             print("Invalid values please try again.")
+
+# def time_in():
+#     time_in = time.asctime(time.localtime(time.time()))
+#     return time_in.
 
 
+class Time:
+    def __init__(self, time, ):
+        self.time = time
+        # self.finish_time = finish_time
+        # self.total_time = total_time
+
+    def start_time(self):
+# time_in = time.asctime(time.localtime(time.time()))
+        self.start_time = time.ctime()
+time_in = Time()
+time_in.start_time()
+    # return time_in
+# timeStruct = time.localtime(time_in)
+# strtime = time.strftime("%Y-%m-%d %H:%M:%S",timeStruct)
+    # def finish_time(self):
+    #     self.finish_time += start_time() 
+
+
+    # def total_time(self):
+        # self.total_time = 
+
+# print(self.start_time())
